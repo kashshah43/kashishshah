7df8948689a9e2f137687176e5d2685472bd4378
kashish.shah <kashshah@vlocity.com>
2020-04-23 15:07:27 -0400

    added about page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('.menu-about').click(function () {
+        $('.menu-about').closest('.overviewOptions').addClass('hideX');
+        $('.aboutContainer').removeClass('hideX');
+    })
+
+});
